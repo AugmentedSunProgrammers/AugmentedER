f6129d9d53bb491474a5765b223dccadf5067635
Patrick <patrick.dolan@selu.edu>
2014-12-05 22:20:06 -0600

    New GUI
    Added the not AR camera that is for all the extra GUIs

---
+++ AR4ER/Assets/DisableCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
